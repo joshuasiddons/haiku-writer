fc7435f0d2703a905bec12c6127ff07152c71c3a
joshuas <64150679+joshuas2112@users.noreply.github.com>
2026-03-02 20:53:50 +0000

    auto resize box implemented

--- client/src/components/HiddenTextArea/HiddenTextArea.jsx
+++ client/src/components/HiddenTextArea/HiddenTextArea.jsx
@@ -1,8 +1,22 @@
 import styles from './HiddenTextArea.module.scss';
 
 function HiddenTextArea({ elementID }) {
+
+    const resizeBox = () => {
+        const box = document.getElementById(elementID);
+        box.style.height = 0;
+        box.style.height = box.scrollHeight + 'px';
+    }
+
     return (
-        <textarea name="input" id={elementID} className={styles.hiddenTextArea} cols="30" rows="10"></textarea>
+        <textarea
+            name='input'
+            id={elementID}
+            className={styles.hiddenTextArea}
+            onInput={resizeBox}
+            cols='30'
+            rows='1'
+        ></textarea>
     )
 }
 
